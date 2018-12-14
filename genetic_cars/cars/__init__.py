cdfe8bc7537d2967eb47a0351cae8bff6c7fc8ed
Daniel Bigos <daniel.bigos96@gmail.com>
2018-12-14 01:06:52 +0100

    docs: add pydoc

--- genetic_cars/cars/__init__.py
+++ genetic_cars/cars/__init__.py
@@ -1,10 +1,11 @@
 """
-Project from ALHE course
-Topic: Generate the best car
+Module providing car methods
+Works with pybox2d
 """
 
-from os import path
+
 import sys
+from os import path
 
 TRIANGLE_GENES = 7
 TRIANGLES = 5
